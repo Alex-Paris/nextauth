fd55f301420e23fb6dbcbe6054ae476310157d55
Alex Paris <alex_paris28@hotmail.com>
2022-08-17 10:56:54 -0300

    chore: guest auto redirect to dash if have token

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
-const Home: NextPage = () => {
+export default function Home() {
   const [email, setEmail] = useState('diego@rocketseat.team');
   const [password, setPassword] = useState('123456');
 
@@ -31,4 +29,21 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  /*const cookies = parseCookies(ctx)
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }*/
+
+  return {
+    props: {
+
+    }
+  }
+})
