4ee836f966630ee515d2a539576bf8d0a054ee86
Alex Paris <alex_paris28@hotmail.com>
2022-08-21 03:43:44 -0300

    chore: Can component and auth element in page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
 
   useEffect(() => {
     api
@@ -15,7 +21,17 @@ export default function Dashboard() {
       .catch(err => console.log(err))
   }, [])
 
-  return <h1>Dashboard: {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas com Hook</div>}
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas com Component</div>
+      </Can>
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
